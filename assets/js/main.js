013c117e47f100f6b7502bdbf939b9558b145ba2
Ng Thanh Cong <nxgthanhcong@gmail.com>
2021-12-14 15:44:46 +0700

    commit code

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,3 +78,58 @@ new Swiper('.testimonial-slider', {
     }
 });
 
+var eWatchInGallery = document.querySelectorAll(".fas.fa-plus");
+var eGallery = document.querySelector(".portfolio__gallery");
+var eGalleryImg = document.querySelector(".portfolio__gallery img");
+var eGalleryCloseBtn = document.querySelector(".gallery-icon.icon-close");
+var eGalleryNextBtn = document.querySelector(".gallery-icon.icon-next");
+var eGalleryPrevBtn = document.querySelector(".gallery-icon.icon-prev");
+var eListImages = document.querySelectorAll(".portfolio__item img");
+
+var currentIndex = 0;
+
+function showGalleryImage(index) {
+    eGalleryImg.src = eListImages[index].src;
+    eGallery.classList.remove("hide");
+}
+
+eWatchInGallery.forEach((item, index) => {
+    item.addEventListener("click", function () {
+        currentIndex = index;
+        showGalleryImage(currentIndex);
+    })
+})
+
+eGalleryNextBtn.addEventListener("click", function () {
+    currentIndex++;
+    if (currentIndex >= eListImages.length)
+        currentIndex = 0;
+    showGalleryImage(currentIndex);
+});
+
+eGalleryPrevBtn.addEventListener("click", function () {
+    currentIndex--;
+    if (currentIndex <= 0)
+        currentIndex = eListImages.length - 1;
+    showGalleryImage(currentIndex);
+});
+
+eGalleryCloseBtn.addEventListener("click", function () {
+    eGallery.classList.add("hide");
+})
+
+var eWatchPortfolioDetail = document.querySelectorAll(".fas.fa-link");
+var ePortfolioDetail = document.querySelector(".portfolio__detail");
+var ePortfolioDetailCloseBtn = document.querySelector(".portfolio__detail-icon.icon-close");
+
+//fas fa-link
+
+eWatchPortfolioDetail.forEach((item) => {
+    item.addEventListener("click", function () {
+        ePortfolioDetail.classList.remove("hide");
+    })
+})
+
+ePortfolioDetailCloseBtn.addEventListener("click", function () {
+    ePortfolioDetail.classList.add("hide");
+})
